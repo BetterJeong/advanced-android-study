73e21cccb83e6fe5112f69a5449a97aec74a97e5
Better Jeong <83108398+BetterJeong@users.noreply.github.com>
2023-09-07 11:38:47 +0900

    클릭 시 클릭한 위치에 그리기

--- week2/MyApplication/app/src/main/java/com/example/myapplication/MyView.kt
+++ week2/MyApplication/app/src/main/java/com/example/myapplication/MyView.kt
@@ -7,23 +7,48 @@ import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
+import android.widget.RadioButton
 
 class MyView : View {
+    private var paint = Paint()
+    private var x = 100F
+    private var y = 100F
+    private var drawMode = 0
+    lateinit var mainActivity: MainActivity
+
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+        mainActivity = context as MainActivity
 
-        val paint = Paint()
-        paint.color = Color.RED
-        canvas.drawRect(100F, 100F, 200F, 200F, paint)
+        paint.color = Color.BLUE
+
+        if (drawMode == 0) {
+            canvas.drawRect(x, y, x + 100, y + 100, paint)
+        }
+        else if (drawMode == 1) {
+            canvas.drawCircle(x, y, 50F, paint)
+        }
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
+        val radioRect = mainActivity.findViewById<RadioButton>(R.id.radioRect)
+        val radioCircle = mainActivity.findViewById<RadioButton>(R.id.radioCircle)
+
         if (event.action == MotionEvent.ACTION_DOWN) {
-            println("${event.x}, ${event.y}")
-            performClick()
+            x = event.x
+            y = event.y
+
+            if (radioRect.isChecked) {
+                drawMode = 0
+            }
+            if (radioCircle.isChecked) {
+                drawMode = 1
+            }
+
+            invalidate()
             return true
         }
         return super.onTouchEvent(event)
